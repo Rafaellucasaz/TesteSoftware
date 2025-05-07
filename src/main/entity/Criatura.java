cffb715da9b764ea6516d0d32db13ab7a0e33901
Rafael Azevedo <rafaellucasaz94@gmail.com>
2025-05-07 10:41:15 -0300

    commit inicial

---
+++ src/main/entity/Criatura.java
@@ -0,0 +1,69 @@
+package main.entity;
+
+import main.Simulacao;
+
+public class Criatura {
+
+    private int id;
+    private int ouro;
+    private double posX;
+
+    public Criatura(int id){
+        this.id = id;
+        this.ouro = 1000000;
+        this.posX = posInicial();
+    }
+
+    public int getOuro() {
+        return ouro;
+    }
+
+    public void setOuro(int ouro) {
+        this.ouro = ouro;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private double posInicial (){
+         return Math.random() * 100;
+    }
+
+    public void move(){
+        this.posX += (Math.random() * 2) - 1;
+    }
+
+    private Criatura criaturaMaisProx(Criatura[] criaturas){
+        Criatura criaturaMaisProx = null;
+        double menorDist = 101;
+        double distAtual;
+        for(int i = 0; i < criaturas.length;i++){
+            if(criaturas[i].id != this.id){
+                distAtual = Math.abs(criaturas[i].posX - this.posX);
+                if(distAtual < menorDist){
+                    criaturaMaisProx = criaturas[i];
+                    menorDist = distAtual;
+                }
+            }
+        }
+        return criaturaMaisProx;
+    }
+    public void roubar(Criatura[] criaturas){
+        Criatura criatura = criaturaMaisProx(criaturas);
+        criatura.ouro = criatura.ouro/2;
+        this.ouro += criatura.ouro;
+    }
+}
